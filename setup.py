0c97e4234840791d6985b3c06a9694e6ef5c7905
Vikash Kumar <19cse217.vikashkumarchaudhary.edu>
2023-03-06 22:32:12 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools find_packeges,setup
+
+
+
+setup(
+    name = 'Ml Project'
+    version = '0.0.1',
+    Author = 'vikash chaudhary',
+    Author_email = "contact.vikashchaudhary@gmail.com",
+    packages = find_packages(),
+    install_requires = ['pandas','numpy','seaborn']
+    
+    
+    
+)
